06a4b940215ae9d1e3a743fc38ac558308043af5
AkankshaAnil <akankshakodgirwar07@gmail.com>
2023-07-15 14:31:05 +0530

    [AkankshaAnil] Adding Functionality to Print Percentage Of Head & Tail

--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -1,7 +1,12 @@
-﻿internal class Program
+﻿using BasicCorePrograms;
+using System;
+internal class Program
 {
-    private static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        Console.WriteLine("The Implementation Is About Basic Core Programming");
+        Console.WriteLine("Flip coin Program");
+        Console.WriteLine("Hello User!\n Please Enter the index no. of the program you want to run.");
+        FlipTheCoin Coin = new FlipTheCoin();
+        Coin.FlipCoin();
     }
 }
